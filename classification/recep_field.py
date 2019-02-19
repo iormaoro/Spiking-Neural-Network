14d7376c5a8e328870d1902d5cd002fa0c8a53c6
iormaoro <Gp3844gp>
2019-02-19 14:32:09 +0100

    Added equation to balance intensity differences between images.

--- classification/recep_field.py
+++ classification/recep_field.py
@@ -7,27 +7,48 @@
 
 import numpy as np
 
-def rf(inp):
-	w = [[-0.5,-0.125,  0.25,  -0.125, -0.5  ],
-	 [-0.125 , 0.25  , 0.625 , 0.25 , -0.125],
-	 [ 0.25   ,0.625 , 1. ,    0.625 , 0.25 ],
-	 [-0.125 , 0.25  , 0.625 , 0.25,  -0.125],
-	 [-0.5  , -0.125 , 0.25 , -0.125 ,-0.5  ]]
-	pot = np.zeros([28,28])
-	ran = [-2,-1,0,1,2]
-	ox = 2
-	oy = 2
 
-	#Convolution
-	for i in range(28):
-		for j in range(28):
-			summ = 0
-			for m in ran:
-				for n in ran:
-					if (i+m)>=0 and (i+m)<=15 and (j+n)>=0 and (j+n)<=15:
-						summ = summ + w[ox+m][oy+n]*inp[i+m][j+n]/255
-			pot[i][j] = summ
-	return pot
+def rf(inp):
+    w=[[-0.5,-0.25,0.25,-0.25,-0.5],
+       [-0.25,0.25,0.625,0.25,-0.25],
+       [0.25,0.625,1.,0.625,0.25],
+       [-0.25,0.25,0.625,0.25,-0.25],
+       [-0.5,-0.25,0.25,-0.25,-0.5]]
+    pot=np.zeros([28,28])
+    ran=[-2,-1,0,1,2]
+    ox=2
+    oy=2
+
+    z=0
+
+    summ_of_summs = 0
+    # Convolution
+    for i in range(28):
+        for j in range(28):
+            summ=0
+            for m in ran:
+                for n in ran:
+                    if (i+m)>=0 and (i+m)<=28-1 and (j+n)>=0 and (j+n)<=28-1:
+                        # if(z>12):
+                        summ=summ+w[ox+m][oy+n] * inp[i+m][j+n] / 255
+                    # else:
+                    #     summ=summ+w[ox+m][oy+n] * inp[i+m][j+n] / 255
+                    # print summ
+            summ_of_summs=summ_of_summs+summ
+
+            # With this z we will control that if there is an image with many white, it won't outpeform the
+            # rest because of it's intensity, it will somehow normalize it for every image.
+            # print summ
+            # if (summ>1.1):
+            #     z=z+1
+            pot[i][j]=summ
+
+    for i in range(28):
+        for j in range(28):
+            # print pot[i][j]
+            pot[i][j] = pot[i][j] + (pot[i][j] * (((50.0 - summ_of_summs)/(summ_of_summs*2))))
+
+    return pot
 
 # if __name__ == '__main__':
 
