01b9754ee209ed6ca57c4276f6c161e60f270f78
iormaoro <Gp3844gp>
2019-03-29 21:54:49 +0100

    img->pot, we were doing wrong classifications! print t gives us when the spike has been given

--- classification/classify.py
+++ classification/classify.py
@@ -19,16 +19,16 @@ T=400
 time=np.arange(1,T+1,1)
 t_back=-20
 t_fore=20
-Pth=6  # Should be Pth = 6 for deterministic spike train
+Pth=2  # Should be Pth = 6 for deterministic spike train
 m=784  # Number of neurons in first layer
-n=4  # Number of neurons in second layer
+n=3  # Number of neurons in second layer
 epoch=1
 num_of_images=6
 w_max=0.5
 w_min=-0.5
 
 # T = 400
-time_const = 10
+time_const = 1
 update_time = 0.1
 
 layer2=[]
@@ -55,12 +55,13 @@ synapse=np.load('weights_numpy.npy')
 
 pruebas = 0
 acertado = 0
-for k in range(0,4):
+for k in range(0,3):
     for i in range(0,8):
+        winner=10
         spike_count=np.zeros((n,1))
 
         # read the image to be classified
-        img=np.array(Image.open("testing_images/"+str(k)+"_"+str(i)+".png"))
+        img=np.array(Image.open("0/"+str(k)+"_"+str(i)+".png"))
         print str(k)+"_"+str(i)+".png"
 
         # initialize the potentials of output neurons
@@ -72,7 +73,7 @@ for k in range(0,4):
 
         # generate spike trains. Select between deterministic and stochastic
         # train = np.array(encode_deterministic(pot))
-        train=np.array(encode_deterministic(img))
+        train=np.array(encode_deterministic(pot))
 
         # flag for lateral inhibition
         f_spike=0
@@ -91,7 +92,9 @@ for k in range(0,4):
             # Lateral Inhibition
             if (f_spike==0):
                 high_pot=max(active_pot)
+                # print high_pot
                 if (high_pot>Pth):
+                    print t
                     f_spike=1
                     winner=np.argmax(active_pot)
                     print "winner is" + str(winner)
